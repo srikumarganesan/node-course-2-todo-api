923e29df779cf1b7248fb1cc73cab703d2fee565
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-29 11:52:59 +0530

    Updated the connection method to get rid of the deprecated warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@
 const mongoose = require(`mongoose`);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost:27017/TodoApp`);
+mongoose.connection.openUri(`mongodb://localhost:27017/TodoApp`);
 
 module.exports = {mongoose};

f521d73680ce13d8c9b8653d89dccd41ae51bc46
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-29 19:29:15 +0530

    updated the connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ const mongoose = require(`mongoose`);
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(`mongodb://localhost:27017/TodoApp`);
-mongoose.connect(`mongodb://srikumar:srikumar@123@ds143362.mlab.com:43362/todoapp`);
+mongoose.connect(`mongodb://srikumar:srikumar@ds143362.mlab.com:43362/todoapp`);
 
 module.exports = {mongoose};

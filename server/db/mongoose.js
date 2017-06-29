d52b283746dbf3b4027d713f93c3d4123290cdc6
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-29 16:45:23 +0530

    Added the mlab mongodb uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,7 @@
 const mongoose = require(`mongoose`);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost:27017/TodoApp`);
+//mongoose.connect(`mongodb://localhost:27017/TodoApp`);
+mongoose.connect(`mongodb://srikumar:srikumar@123@ds143362.mlab.com:43362/todoapp`);
 
 module.exports = {mongoose};

b671830e8ab4ccd6d81f3d87181e0eafaf005f00
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-29 16:45:52 +0530

    Added the port constant

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const {Todo} = require(`./models/todo`);
 const {User} = require(`./models/user`);
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,6 +47,6 @@ app.get(`/todos/:id`,(req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
-app.listen(3000, () => console.log(`Started server on port 3000`));
+app.listen(port, () => console.log(`Started server on port ${port}`));
 
 module.exports = {app};

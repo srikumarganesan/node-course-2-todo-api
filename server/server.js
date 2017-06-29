936e9d724913de6d170f21257ea6f3821b3de541
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-29 15:27:29 +0530

    Added the logic for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
  */
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
+const {ObjectID} = require(`mongodb`);
 
 const {mongoose} = require(`./db/mongoose`);
 const {Todo} = require(`./models/todo`);
@@ -30,6 +31,19 @@ app.get(`/todos`, (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get(`/todos/:id`,(req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => console.log(`Started server on port 3000`));

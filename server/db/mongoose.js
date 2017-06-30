16240a0f7b7cd3060bc88966bebec8f363f3d064
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-30 21:18:00 +0530

    Removed the hardcoded mongo connect url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,6 @@
 const mongoose = require(`mongoose`);
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(`mongodb://localhost:27017/TodoApp`);
-mongoose.connect(`mongodb://srikumar:srikumar@ds143362.mlab.com:43362/todoapp`);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};

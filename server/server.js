dcbe7cfaedd4428a072e3bbe1dde7f5bdd30a958
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-30 09:59:37 +0530

    Added the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+//POST endpoint
 app.post(`/todos`, (req, res) => {
     let todo = new Todo({
         text: req.body.text
@@ -26,6 +27,7 @@ app.post(`/todos`, (req, res) => {
     });
 });
 
+//GET endpoint, Gets all the todos
 app.get(`/todos`, (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
@@ -34,12 +36,27 @@ app.get(`/todos`, (req, res) => {
     });
 });
 
+//GET endpoint to get a particular todo
 app.get(`/todos/:id`,(req, res) => {
     let id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
+});
+
+//DELETE endpoint to delete a particular todo
+app.delete(`/todos/:id`, (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }

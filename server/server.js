99a3c0de3567b8122a1c8af9261bfaaef96da66c
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-06-30 19:44:07 +0530

    Added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const {ObjectID} = require(`mongodb`);
+const _ = require(`lodash`);
 
 const {mongoose} = require(`./db/mongoose`);
 const {Todo} = require(`./models/todo`);
@@ -57,6 +58,30 @@ app.delete(`/todos/:id`, (req, res) => {
         return res.status(404).send();
     }
     Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
+});
+
+//PUT endpoint to edit a todo
+app.patch(`/todos/:id`, (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, [`text`, `completed`]);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }

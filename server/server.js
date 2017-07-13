63316253b3c322b4910f774dce8aad453d74cd2d
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-07-13 15:13:39 +0530

    Added POST /users route

--- server/server.js
+++ server/server.js
@@ -91,6 +91,20 @@ app.patch(`/todos/:id`, (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+//POST /users
+app.post(`/users`, (req, res) => {
+    let body = _.pick(req.body, [`email`, `password`]);
+    let user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+        // res.send(doc);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => res.status(400).send(e));
+});
+
+
 app.listen(port, () => console.log(`Started server on port ${port}`));
 
 module.exports = {app};

7dedd834cbb479f507fffbe8e5be1bf6d2a1d5b1
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-07-13 15:12:53 +0530

    Added tokens for user schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,15 +2,57 @@
  * Created by GSrikumar on 6/28/2017.
  */
 const mongoose = require(`mongoose`);
+const validator = require(`validator`);
+const jwt = require(`jsonwebtoken`)
+const _ = require(`lodash`);
 
-
-let User = mongoose.model(`Users`,{
+let UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength:4,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: `{VALUE} is not a valid email`
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token: {
+            type: String,
+            require: true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+    let user = this;
+    let access = `auth`;
+    let token = jwt.sign({ _id: user._id.toHexString(), access},`abc123`).toString();
+
+    user.tokens.push({access, token});
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+let User = mongoose.model(`Users`,UserSchema);
 
 module.exports = {User};

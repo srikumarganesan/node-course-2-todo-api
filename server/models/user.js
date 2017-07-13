549efb130cc0af808c387df6c34e6fde6b3bb063
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-07-13 20:04:54 +0530

    Added the findByToken Model method

--- server/models/user.js
+++ server/models/user.js
@@ -35,12 +35,33 @@ let UserSchema = new mongoose.Schema({
     }]
 });
 
+//Instance method
 UserSchema.methods.toJSON = function() {
     let user = this;
     let userObject = user.toObject();
 
     return _.pick(userObject, ['_id', 'email']);
 };
+
+//Model method
+UserSchema.statics.findByToken = function(token) {
+    let User = this;
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, `abc123`)
+    } catch (e) {
+        return Promise.reject();
+    }
+
+    return User.findOne({
+       '_id': decoded._id,
+        //Use quotes when we have dot in the key
+        'tokens.token': token,
+        'tokens.access': `auth`
+    });
+};
+
 
 UserSchema.methods.generateAuthToken = function() {
     let user = this;

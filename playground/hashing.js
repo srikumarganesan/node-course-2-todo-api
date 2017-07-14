ceeccd1d04189e563ec5d235e6fb34daa24fff7f
Srikumar Ganesan <srikumarganesan@gmail.com>
2017-07-14 15:20:56 +0530

    Tried bcryptjs

--- playground/hashing.js
+++ playground/hashing.js
@@ -3,16 +3,26 @@
  */
 const {SHA256} = require(`crypto-js`);
 const jwt = require(`jsonwebtoken`);
+const bcrypt = require(`bcryptjs`);
 
-let data = {
-    id: 4
-};
+let password = `123abc!`;
 
-let token = jwt.sign(data, `123`);
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => console.log(hash));
+// });
 
-let decoded = jwt.verify(token, `123`);
-console.log(`Decoded: `, decoded);
+var hashedPassword = `$2a$10$sEDsJBGkNwwxCMAihJpd3ONB9hynluGFLltjienaaasQfIxR1dLa2`;
+
+bcrypt.compare(password, hashedPassword, (err, result) => console.log(result));
+// let data = {
+//     id: 4
+// };
+//
+// let token = jwt.sign(data, `123`);
+// console.log(token);
+//
+// let decoded = jwt.verify(token, `123`);
+// console.log(`Decoded: `, decoded);
 
 // let message = `I am user number 3`;
 // let hash = SHA256(message).toString();

4235f1fb844b44bff3e3026c503776ef14e529a5
Srikumar Ganesan <srikumarganesan@gmail.com>
2018-06-06 21:18:43 +0530

    Added CORS

--- server/server.js
+++ server/server.js
@@ -18,6 +18,11 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    next();
+});
+
 //POST endpoint
 app.post(`/todos`, (req, res) => {
     let todo = new Todo({
